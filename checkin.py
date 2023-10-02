cbbbc082193c9f1d322de48e430b1d120cb6d8c5
HaoZi <38182473+ToumaLover@users.noreply.github.com>
2023-10-02 23:18:24 +0800

    Update2  checkin.py

--- checkin.py
+++ checkin.py
@@ -26,7 +26,7 @@ for cookie_line in cookie_lines:
     signing_cookie = sys.argv[2].strip()
     headers = {
         "user-agent": "Skland/1.0.1 (com.hypergryph.skland; build:100001014; Android 25; ) Okhttp/4.11.0",
-        "cred": signing_cookie
+        "cred": signing_cookie,
         "vName": "1.0.1",
         "vCode": "100001014",
         'Accept-Encoding': 'gzip',
